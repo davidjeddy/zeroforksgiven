9573392d0a108ff305517c24306cee29379b8d01
David J Eddy <me@davidjeddy.com>
2016-08-13 14:50:04 -0400

    ADDED resutoran/backend to App backend modules

--- backend/config/web.php
+++ backend/config/web.php
@@ -37,6 +37,10 @@ $config = [
         'i18n' => [
             'class' => 'backend\modules\i18n\Module',
             'defaultRoute'=>'i18n-message/index'
+        ],
+        'resutoran' => [
+            'class'         => 'resutoran\backend\Module',
+            'defaultRoute'  => 'default/index'
         ]
     ],
     'as globalAccess'=>[
